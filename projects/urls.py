bd84fb1b60ba327b75891317a6117dbd95081ff8
Eraldo Helal <eraldo@eraldo.org>
2014-06-16 09:50:13 +0200

    added project crud functionality

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from projects.views import ProjectListView
+from projects.views import ProjectListView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView, ProjectDetailView
 
 __author__ = 'eraldo'
 
@@ -9,4 +9,24 @@ urlpatterns = patterns('',
     url(r'^$',
         ProjectListView.as_view(),
         name='list'),
+
+    # ex: ../create/
+    url(r'^create/$',
+        ProjectCreateView.as_view(),
+        name='create'),
+
+        # ex: ../4/
+    url(r'^(?P<pk>\d+)/$',
+        ProjectDetailView.as_view(),
+        name='detail'),
+
+    # ex: ../4/update/
+    url(r'^(?P<pk>\d+)/update/$',
+        ProjectUpdateView.as_view(),
+        name='update'),
+
+    # ex: ../4/delete/
+    url(r'^(?P<pk>\d+)/delete/$',
+        ProjectDeleteView.as_view(),
+        name='delete'),
     )
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import ListView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from projects.models import Project
+
+__author__ = 'eraldo'
 
 
 class ProjectListView(ListView):
     model = Project
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    success_url = reverse_lazy('projects:list')
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    success_url = reverse_lazy('projects:list')
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    success_url = reverse_lazy('projects:list')
