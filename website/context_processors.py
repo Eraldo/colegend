950f0f33b38a25511962bc1055cefefcf59b6b4b
Eraldo Helal <eraldo@eraldo.org>
2014-06-27 14:30:31 +0200

    refactored menu to show item name and icon

--- website/context_processors.py
+++ website/context_processors.py
@@ -1,6 +1,19 @@
 from bootstrap3.components import render_icon
+from django.utils.safestring import mark_safe
 
 __author__ = 'eraldo'
+
+
+class MenuItem:
+    def __init__(self, name, url, icon=None):
+        self.name = name
+        self.url = url
+        if icon:
+            icon = mark_safe(render_icon(icon))
+        self.icon = icon
+
+    def __str__(self):
+        return self.name
 
 
 def menu(request):
@@ -8,13 +21,13 @@ def menu(request):
 
     return {
         # name, url
-        'menu_items': (
-            (render_icon("home"), 'home'),
-            (render_icon("briefcase"), 'projects:project_list'),
-            (render_icon("check"), 'tasks:task_list'),
-            (render_icon("tags"), 'tags:tag_list'),
-            (render_icon("eye-open"), 'test'),
-            (render_icon("asterisk"), 'commands'),
-        )
+        'menu_items': [
+            MenuItem("home", url="home", icon="home"),
+            MenuItem("projects", url="projects:project_list", icon="briefcase"),
+            MenuItem("tasks", url="tasks:task_list", icon="check"),
+            MenuItem("tags", url="tags:tag_list", icon="tags"),
+            MenuItem("test", url="test", icon="eye-open"),
+            MenuItem("commands", url="commands", icon="asterisk"),
+        ]
     }
 
