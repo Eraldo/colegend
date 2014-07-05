df1322ef35b99d655cb0e6f5ee0709a2a32c2e75
Eraldo Helal <eraldo@eraldo.org>
2014-07-05 12:24:11 +0200

    updated messages style

--- cogame/website/views.py
+++ cogame/website/views.py
@@ -1,7 +1,15 @@
+from django.contrib import messages
 from django.views.generic import TemplateView
 
 __author__ = 'eraldo'
 
 
 class TestView(TemplateView):
-    template_name = "website/local_test.html"
+    template_name = "website/test.html"
+
+    def get(self, request, *args, **kwargs):
+        message = "test1"
+        messages.add_message(request, messages.INFO, message)
+        message = "test2"
+        messages.add_message(request, messages.INFO, message)
+        return super(TestView, self).get(request, *args, **kwargs)
