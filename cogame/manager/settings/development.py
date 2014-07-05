afe0e3b22abe6dd4549012f8d37513519c367813
Eraldo Helal <eraldo@eraldo.org>
2014-07-05 12:08:21 +0200

    fixed settings

--- cogame/manager/settings/development.py
+++ cogame/manager/settings/development.py
@@ -41,15 +41,15 @@ CACHES = {
 INSTALLED_APPS += (
     'debug_toolbar',
 )
-
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-# http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
-INTERNAL_IPS = ('127.0.0.1',)
+#
+# MIDDLEWARE_CLASSES += (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
+#
+# DEBUG_TOOLBAR_PATCH_SETTINGS = False
+#
+# # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
+# INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
 
 
