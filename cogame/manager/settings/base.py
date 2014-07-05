6e0d7e8b0de79c63a4a99d6d9caaae58b3c2d1e8
Eraldo Helal <eraldo@eraldo.org>
2014-07-05 18:23:12 +0200

    removed sites app

--- cogame/manager/settings/base.py
+++ cogame/manager/settings/base.py
@@ -202,7 +202,6 @@ DJANGO_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
