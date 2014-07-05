01c98e84fbf782b83d090df5b7888c1b53b12a51
Eraldo Helal <eraldo@eraldo.org>
2014-07-05 21:08:51 +0200

    added basic global search feature

--- cogame/website/urls.py
+++ cogame/website/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
-from website.views import TestView
+from website.views import TestView, SearchView
 
 __author__ = 'eraldo'
 
 
 urlpatterns = patterns('',
+    url(r'^search/$',
+        SearchView.as_view(),
+        name='search'),
     url(r'^test/$',
         TestView.as_view(),
         name='test'),
--- cogame/website/views.py
+++ cogame/website/views.py
@@ -1,5 +1,8 @@
 from django.contrib import messages
 from django.views.generic import TemplateView
+from projects.models import Project
+from tags.models import Tag
+from tasks.models import Task
 
 __author__ = 'eraldo'
 
@@ -13,3 +16,16 @@ class TestView(TemplateView):
         message = "test2"
         messages.add_message(request, messages.INFO, message)
         return super(TestView, self).get(request, *args, **kwargs)
+
+
+class SearchView(TemplateView):
+    template_name = 'website/search.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchView, self).get_context_data(*args, **kwargs)
+        q = self.request.GET.get("q")
+        if q:
+            context["projects"] = Project.objects.filter(name__icontains=q)
+            context["tasks"] = Task.objects.filter(name__icontains=q)
+            context["tags"] = Tag.objects.filter(name__icontains=q)
+        return context
