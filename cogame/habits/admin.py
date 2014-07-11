385876cfe309ecee6f8292f78adbbf93bdb62619
Eraldo Helal <eraldo@eraldo.org>
2014-07-11 16:01:06 +0200

    added admin to habits app

--- cogame/habits/admin.py
+++ cogame/habits/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
+from habits.models import Habit
+from lib.admin import InlineMixin
 
 __author__ = 'eraldo'
 
 
-# Register your models here.
+class HabitInline(InlineMixin, admin.TabularInline):
+    model = Habit
+    fields = ['name', 'tags', 'history', 'change_link']
+    extra = 0
+    readonly_fields = ['change_link']
+
+
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name', 'description']
+    list_filter = ['tags']
+    filter_horizontal = ['tags']
+    readonly_fields = ['creation_date', 'modification_date', 'history']
+
+    fieldsets = [
+        (None, {'fields': ['name', 'description']}),
+        (None, {'fields': ['tags']}),
+        ('history', {'fields': ['creation_date', 'modification_date', 'history'], 'classes': ['collapse']}),
+    ]
+
+
+admin.site.register(Habit, HabitAdmin)
