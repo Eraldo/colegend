83dd9e5d4d21f216fd8dabba1b35453b1d72c883
Eraldo Helal <eraldo@eraldo.org>
2014-07-16 00:05:13 +0200

    updated requirements

--- cogame/configuration/urls.py
+++ cogame/configuration/urls.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-
 admin.autodiscover()
 
 # remove group model from admin
@@ -26,11 +27,14 @@ urlpatterns = patterns('',
     url(r'^features/', include('features.urls', namespace="features")),
     url(r'^commands/', include('commands.urls')),
 
+    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
 
-from django.conf import settings
+    # User management
+    url(r'^users/', include("users.urls", namespace="users")),
+    url(r'^accounts/', include('allauth.urls')),
 
-if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Uncomment the next line to enable avatars
+    #url(r'^avatar/', include('avatar.urls')),
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
