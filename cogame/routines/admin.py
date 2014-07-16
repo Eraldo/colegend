1625a91c6880ecbf8c348bea124fa8f58f78ff4b
Eraldo Helal <eraldo@eraldo.org>
2014-07-16 15:05:22 +0200

    added owner to routines

--- cogame/routines/admin.py
+++ cogame/routines/admin.py
@@ -6,16 +6,19 @@ __author__ = 'eraldo'
 
 
 class RoutineAdmin(admin.ModelAdmin):
-    list_display = ['name', 'type']
+    list_display = ['name', 'type', 'owner']
     search_fields = ['name', 'description']
-    list_filter = ['type', 'tags']
+    list_filter = ['type', 'tags', 'owner']
     filter_horizontal = ['tags']
     readonly_fields = ['creation_date', 'modification_date', 'history']
 
     fieldsets = [
+        (None, {'fields': ['owner']}),
         (None, {'fields': ['name', 'description']}),
         (None, {'fields': ['type', 'tags']}),
         ('history', {'fields': ['creation_date', 'modification_date', 'history'], 'classes': ['collapse']}),
-        ]
+    ]
     inlines = [HabitInline]
+
+
 admin.site.register(Routine, RoutineAdmin)
--- cogame/routines/models.py
+++ cogame/routines/models.py
@@ -1,20 +1,21 @@
 from django.db import models
-from lib.models import TrackedBase, AutoUrlMixin
+from lib.models import TrackedBase, AutoUrlMixin, OwnedBase
 from tags.models import TaggableBase
 
 __author__ = 'eraldo'
 
 
 class RoutineManager(models.Manager):
-    def get_by_natural_key(self, name):
-        return self.get(name=name)
+    def get_by_natural_key(self, owner, name):
+        return self.get(owner=owner, name=name)
 
 
-class Routine(AutoUrlMixin, TrackedBase, TaggableBase, models.Model):
+class Routine(AutoUrlMixin, OwnedBase, TrackedBase, TaggableBase, models.Model):
     """
     A django model representing a routine.
     """
-    name = models.CharField(max_length=100, unique=True)
+    # > owner: User
+    name = models.CharField(max_length=100)
 
     description = models.TextField(blank=True)
 
@@ -35,9 +36,12 @@ class Routine(AutoUrlMixin, TrackedBase, TaggableBase, models.Model):
 
     class Meta:
         ordering = ["name"]
+        unique_together = (('owner', 'name'),)
 
     def __str__(self):
         return self.name
 
     def natural_key(self):
-        return [self.name]
+        return [self.owner, self.name]
+
+    natural_key.dependencies = ['users.user']
