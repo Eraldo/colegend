09c39b58b760024414310719ab3cb01e61da22d3
Eraldo Helal <eraldo@eraldo.org>
2014-07-16 05:21:34 +0200

    updated user avatar integration

--- cogame/home/views.py
+++ cogame/home/views.py
@@ -1,7 +1,14 @@
+from braces.views import LoginRequiredMixin
 from django.views.generic import TemplateView
+from features.models import Feature
 
 __author__ = 'eraldo'
 
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "home/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['feature'] = Feature.objects.first()
+        return context
