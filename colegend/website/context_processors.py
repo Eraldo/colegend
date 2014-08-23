28be6b8068d84514549f4d4341c4f29b5847f176
Eraldo Helal <eraldo@eraldo.org>
2014-08-24 00:03:16 +0200

    Added font awesome icons.

--- colegend/website/context_processors.py
+++ colegend/website/context_processors.py
@@ -1,4 +1,3 @@
-from bootstrap3.components import render_icon
 from django.utils.safestring import mark_safe
 
 __author__ = 'eraldo'
@@ -12,7 +11,7 @@ class MenuItem:
         self.url = url
         # add icon if given
         if icon:
-            icon = mark_safe(render_icon(icon))
+            icon = mark_safe('<i class="fa fa-{}"></i>'.format(icon))
         self.icon = icon
 
     def __str__(self):
@@ -23,15 +22,15 @@ def menu(request):
     """A context processor that provides menu_items."""
     menu_items = {
         'main': [
-            MenuItem("Projects", url="projects:project_list", icon="folder-open"),
-            MenuItem("Tasks", url="tasks:task_list", icon="check"),
+            MenuItem("Projects", url="projects:project_list", icon="sitemap"),
+            MenuItem("Tasks", url="tasks:task_list", icon="check-circle"),
             MenuItem("Tags", url="tags:tag_list", icon="tags"),
         ],
         'extra': [
             MenuItem("Home", url="home", icon="home"),
-            MenuItem("About", url="about", icon="info-sign"),
-            MenuItem("Meetings", url="meetings", icon="map-marker"),
-            MenuItem("vision", url="visions:vision_list", icon="eye-open"),
+            MenuItem("About", url="about", icon="info-circle"),
+            MenuItem("Meetings", url="meetings", icon="comments-o"),
+            MenuItem("vision", url="visions:vision_list", icon="eye"),
             MenuItem("Feature Roadmap", url="features:feature_list", icon="road"),
         ],
         'settings': [
@@ -39,12 +38,12 @@ def menu(request):
             MenuItem("contact", url="home", icon="envelope"),
         ],
         'experimental': [
-            MenuItem("routines", url="routines:routine_list", icon="asterisk"),
+            MenuItem("routines", url="routines:routine_list", icon="stack-overflow"),
             MenuItem("habits", url="habits:habit_list", icon="link"),
             MenuItem("commands", url="commands", icon="bullhorn"),
         ],
         'admin': [
-            MenuItem("test", url="test", icon="eye-open"),
+            MenuItem("test", url="test", icon="code"),
         ],
     }
     return {'menu_items': menu_items}
