25cc0186ceb227a4cdf242a9016b5fdc27aa5059
Eraldo Helal <eraldo@eraldo.org>
2014-09-12 02:30:25 +0200

    Fixed phone number validation.

--- colegend/lib/modelfields.py
+++ colegend/lib/modelfields.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db.models import CharField
 from django.utils.translation import ugettext_lazy as _
 from lib.validators import PhoneValidator
@@ -8,9 +9,8 @@ __author__ = 'eraldo'
 class PhoneField(CharField):
     description = _("Phone number in the format: +XXXXXXXXXXXXXXX (up to %(max_length)s numbers)")
 
-    default_validators = [PhoneValidator()]
+    default_validators = [PhoneValidator(), MinValueValidator(10)]
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 16
-        kwargs['min_length'] = 10
         super(PhoneField, self).__init__(*args, **kwargs)
