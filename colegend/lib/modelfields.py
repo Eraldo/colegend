2032fbc24f86e6056531d1513d279f7fa659c8d8
Eraldo Helal <eraldo@eraldo.org>
2014-09-12 15:34:47 +0200

    Fixed PhoneValidator.
    Was using value instead of length comparison.

--- colegend/lib/modelfields.py
+++ colegend/lib/modelfields.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinLengthValidator
 from django.db.models import CharField
 from django.utils.translation import ugettext_lazy as _
 from lib.validators import PhoneValidator
@@ -9,7 +9,7 @@ __author__ = 'eraldo'
 class PhoneField(CharField):
     description = _("Phone number in the format: +XXXXXXXXXXXXXXX (up to %(max_length)s numbers)")
 
-    default_validators = [PhoneValidator(), MinValueValidator(10)]
+    default_validators = [PhoneValidator(), MinLengthValidator(10)]
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 16
