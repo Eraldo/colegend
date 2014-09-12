286f66477ded37ee8e89ae34f293125ea5b9e652
Eraldo Helal <eraldo@eraldo.org>
2014-09-12 23:52:28 +0200

    Added a custom user manager.

---
+++ colegend/users/managers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import BaseUserManager
+from django.utils import timezone
+
+__author__ = 'eraldo'
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, username, password,
+                     is_staff, is_superuser, is_active=False, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        now = timezone.now()
+        if not username:
+            raise ValueError('The given username must be set')
+        user = self.model(username=username,
+                          is_staff=is_staff, is_active=is_active,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        return self._create_user(username, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, username, password, **extra_fields):
+        return self._create_user(username, password, True, True,
+                                 is_active=True, **extra_fields)
