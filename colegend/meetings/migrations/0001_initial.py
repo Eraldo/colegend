9f0e832624d178fe7f6fb0864ce865ebbfdb6a2a
Eraldo Helal <eraldo@eraldo.org>
2014-09-13 13:14:08 +0200

    Added initial migrations for tags, statuses and meetings app.

---
+++ colegend/meetings/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Meeting',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('date', models.DateTimeField(null=True, blank=True)),
+            ],
+            options={
+                'ordering': ['date'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
