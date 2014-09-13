5f68191b8863e93eca3c595c306cbbee054f406b
Eraldo Helal <eraldo@eraldo.org>
2014-09-13 12:43:51 +0200

    Added mixin to make emails in admin clickable.

--- colegend/lib/admin.py
+++ colegend/lib/admin.py
@@ -8,14 +8,26 @@ class InlineMixin:
         if not instance.id:
             return "(save and then edit)"
         url = reverse('admin:%s_%s_change' % (
-            instance._meta.app_label,  instance._meta.module_name),  args=[instance.id])
+            instance._meta.app_label, instance._meta.module_name), args=[instance.id])
         return u'<a href="{u}">Edit</a>'.format(u=url)
+
     change_link.allow_tags = True
-    change_link.short_description = 'edit?'   # omit column header
+    change_link.short_description = 'edit?'  # omit column header
 
     from django.forms import TextInput, Textarea
     from django.db import models
+
     formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
-        models.TextField: {'widget': Textarea(attrs={'rows':1, 'cols':40})},
+        models.CharField: {'widget': TextInput(attrs={'size': '20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 40})},
     }
+
+
+class EmailMixin:
+    def email_link(self, obj):
+        return '<a href="{0}{1}">{1}</a>'.format("mailto:", obj.email)
+    email_link.allow_tags = True
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super(EmailMixin, self).get_readonly_fields(request, obj=None)
+        return readonly_fields + ('email_link',)
