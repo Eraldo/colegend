17471deb8089ed7c5e5b84e1053a8d8db9ccb440
Eraldo Helal <eraldo@eraldo.org>
2014-09-22 23:21:08 +0200

    Fixed contact page avatar to show a fixed avatar instead of the current user.

--- colegend/contact/views.py
+++ colegend/contact/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.views.generic import FormView
 from contact.forms import ContactForm
 
@@ -16,3 +17,13 @@ class ContactView(FormView):
         form.send_email(self.request.user)
         messages.success(self.request, 'Your message has been sent.')
         return super(ContactView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        user_model = get_user_model()
+        try:
+            eraldo = user_model.objects.get(username="Eraldo")
+        except user_model.DoesNotExist:
+            eraldo = None
+        context['eraldo'] = eraldo
+        return context
