dec33956337b23d8d35c22b6cfcba9821862b00b
Eraldo Helal <eraldo@eraldo.org>
2014-09-22 23:07:41 +0200

    Restricted access to legend page to active users.

--- colegend/legend/views.py
+++ colegend/legend/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 from legend.models import Block
+from lib.views import ActiveUserRequiredMixin
 
 
-class LegendView(TemplateView):
+class QuoteMixin(ActiveUserRequiredMixin):
+    pass
+
+
+class LegendView(QuoteMixin, TemplateView):
     template_name = "legend/legend.html"
 
     def get_context_data(self, **kwargs):
