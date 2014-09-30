da7397791dbbb0038712227b14a61a841f8d1d13
Eraldo Helal <eraldo@eraldo.org>
2014-09-30 18:19:27 +0200

    Added a dummy test for journal entries.

--- colegend/journals/tests.py
+++ colegend/journals/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+__author__ = 'eraldo'
+
+
+class DayEntryModelTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_load_initial_custom_template_text(self):
+        """
+        Make sure that the custom journal entry template from the user settings
+        get loaded as the initial text when creating a new journal entry.
+        """
+        self.fail("TODO: Write test.")
