aca6ef7fdc3959d0ffed0922e4a4c3b343a68e8d
Eraldo Helal <eraldo@eraldo.org>
2014-10-16 15:42:49 +0200

    Fixed journal location field migration issue.

--- colegend/journals/migrations/0004_auto_20141016_1541.py
+++ colegend/journals/migrations/0004_auto_20141016_1541.py
@@ -2,19 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import journals.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('journals', '0002_auto_20140908_2030'),
+        ('journals', '0003_dayentry_location'),
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.AlterField(
             model_name='dayentry',
             name='location',
-            field=models.CharField(default='', max_length=100),
-            preserve_default=True,
+            field=models.CharField(max_length=100, default=journals.models.get_last_location),
         ),
     ]
