1f185ec709c9ca8e74777e1186d42552df29ab43
Eraldo Helal <eraldo@eraldo.org>
2014-11-03 13:51:55 +0100

    Added bulk acceptance method to quote admin actions.

--- colegend/quotes/admin.py
+++ colegend/quotes/admin.py
@@ -4,9 +4,17 @@ from django.contrib import admin
 from quotes.models import Quote, Category
 
 
+def accept(modeladmin, request, queryset):
+    queryset.update(accepted=True)
+
+
+accept.short_description = "Accept selected quotes."
+
+
 class QuoteAdmin(admin.ModelAdmin):
     list_display = ['name', 'author', 'category', 'accepted', 'used_as_daily', 'provider']
     list_filter = ['category', 'author']
+    actions = [accept]
 
 
 admin.site.register(Quote, QuoteAdmin)
