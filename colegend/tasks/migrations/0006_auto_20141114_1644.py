7e67239eb238cb4ece620adb9896a5fc3119fd8b
Eraldo Helal <eraldo@eraldo.org>
2014-11-14 16:45:23 +0100

    Added tag help text migration.

---
+++ colegend/tasks/migrations/0006_auto_20141114_1644.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tasks', '0005_auto_20140924_0010'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='task',
+            name='tags',
+            field=models.ManyToManyField(null=True, blank=True, related_name='tasks', to='tags.Tag', help_text="<a href='/tags/new/' target='_blank'><i class='fa fa-plus-circle' style='color: green;'></i> New Tag</a>- <small>*Refresh page to view new tag.</small><br>"),
+        ),
+    ]
