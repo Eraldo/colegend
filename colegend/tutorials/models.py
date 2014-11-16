a6cf01d89cbb476434bc52d085437eaee98b5a81
Eraldo Helal <eraldo@eraldo.org>
2014-11-16 05:17:43 +0100

    Added markup to tutorials.

--- colegend/tutorials/models.py
+++ colegend/tutorials/models.py
@@ -1,5 +1,6 @@
 from annoying.functions import get_object_or_None
 from django.db import models
+from markitup.fields import MarkupField
 from lib.models import AutoUrlMixin
 
 __author__ = 'eraldo'
@@ -7,7 +8,7 @@ __author__ = 'eraldo'
 
 class Tutorial(AutoUrlMixin, models.Model):
     name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(help_text="What is the idea? How to use the current implementation.")
+    description = MarkupField(help_text="What is the idea? How to use the current implementation.")
 
     def __str__(self):
         return self.name
