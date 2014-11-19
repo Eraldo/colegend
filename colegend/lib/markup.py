13746754b49f455ed664bb0cc4e1f77f9fba643c
Eraldo Helal <eraldo@eraldo.org>
2014-11-19 13:45:42 +0100

    Added youtube embed feature to markup fields.

--- colegend/lib/markup.py
+++ colegend/lib/markup.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse, parse_qs
 from markdown import markdown
 from mdx_linkify.mdx_linkify import LinkifyExtension
 
@@ -9,10 +10,31 @@ def set_target_blank(attrs, new=False):
     return attrs
 
 
+def embed_youtube(attrs, new=False):
+    url = attrs['href']
+    p = urlparse(url)
+    if p.netloc in ['www.youtube.com']:
+        youtube_id = parse_qs(p.query).get('v', [''])[0]
+        embed = parse_qs(p.query).get('embed', [''])[0]
+        html = """
+            <iframe class="embed-responsive-item" src="//www.youtube.com/embed/{}"
+                frameborder="0" allowfullscreen>
+            </iframe>
+            """.format(youtube_id)
+        if embed:  # make it responsive
+            html = """
+                <div class="embed-responsive embed-responsive-16by9">
+                    {}
+                </div>
+                """.format(html)
+        attrs['_text'] = html
+    return attrs
+
+
 def markup(text, *args, **kwargs):
     return markdown(text, *args, safe_mode=True,
                     extensions=['nl2br', 'sane_lists', 'extra',
                                 LinkifyExtension(
-                                    configs={"linkify_callbacks": [set_target_blank]}
+                                    configs={"linkify_callbacks": [set_target_blank, embed_youtube]}
                                 )],
                     **kwargs)
