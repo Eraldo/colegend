77c0874685c891cdb04cf56719ebc7abc94f5492
Eraldo Helal <eraldo@eraldo.org>
2014-11-25 05:21:40 +0100

    Trying to add saucelabs to travis.

--- colegend/website/tests.py
+++ colegend/website/tests.py
@@ -1,6 +1,5 @@
 from time import sleep
 from unittest import skip
-from django.test import TestCase
 from django.test import LiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 from users.models import User
@@ -18,24 +17,22 @@ class ViewTests(LiveServerTestCase):
         cls.selenium.quit()
         super(ViewTests, cls).tearDownClass()
 
-    @skip("TODO: fix to work with travis.")
+    def setUp(self):
+        self.user = UserFactory(is_superuser=True, is_staff=True)
+
+    # @skip("TODO: fix to work with travis.")
     def test_login(self):
-        tester = User.objects.create_user(username="Tester", password="tester")
         self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
         username_input = self.selenium.find_element_by_name("login")
-        username_input.send_keys(tester.username)
-        # username_input.send_keys("tester")
+        username_input.send_keys(self.user.username)
         password_input = self.selenium.find_element_by_name("password")
         password_input.send_keys("tester")
         self.selenium.find_element_by_name("login_submit").click()
-        # sleep(20)
-        # self.selenium.save_screenshot("/Users/eraldo/inbox/test.png")
+        sleep(20)
 
     def test_can_view_admin_site(self):
-        UserFactory(is_superuser=True, is_staff=True)
         # Gertrude opens her web browser, and goes to the admin page
         self.selenium.get(self.live_server_url + '/backend/')
-
         # She sees the familiar 'Django administration' heading
         body = self.selenium.find_element_by_tag_name('body')
         self.assertIn('CoLegend backend', body.text)
