3fafc8f762550a75772c3dcc9693d3e8f1b4e4db
Eraldo Helal <eraldo@eraldo.org>
2014-11-26 11:54:48 +0100

    Split tests into view classes.

--- colegend/tags/tests.py
+++ colegend/tags/tests.py
@@ -9,6 +9,7 @@ __author__ = 'eraldo'
 
 
 class TagFactory(factory.DjangoModelFactory):
+
     class Meta:
         model = Tag
         django_get_or_create = ('name',)
@@ -28,26 +29,28 @@ class TagModelTests(TestCase):
         self.assertEqual(str(tag_new), "tag1")
 
 
-class TagViewTests(TestCase):
+class LoggedInTestMixin():
 
     def setUp(self):
-        pass
-        # TODO: refactor out user creation
+        user = UserFactory()
+        self.client.login(username=user.username, password="tester")
+        self.user = user
+
+
+class TagListViewTest(LoggedInTestMixin, TestCase):
 
     def test_tag_list_view(self):
-        user = UserFactory()
-        tag = TagFactory(owner=user)
-        self.client.login(username=user.username, password="tester")
+        tag = TagFactory(owner=self.user)
         url = reverse("tags:tag_list")
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertIn(tag, response.context['object_list'])
 
+
+class TagNewViewTest(LoggedInTestMixin, TestCase):
+
     def test_tag_new_view(self):
-        user = UserFactory()
-        # tag = TagFactory(owner=user)
-        self.client.login(username=user.username, password="tester")
         url = reverse("tags:tag_new")
         response = self.client.get(url)
 
@@ -56,39 +59,42 @@ class TagViewTests(TestCase):
 
     def test_tag_new_view_saving_a_post_request(self):
         tag_attributes = TagFactory.attributes()
-        self.client.login(username=tag_attributes["owner"].username, password="tester")
+        tag_attributes["owner"] = self.user
         url = reverse("tags:tag_new")
         response = self.client.post(url, data=tag_attributes)
+
         self.assertEqual(Tag.objects.count(), 1)
         new_tag = Tag.objects.first()
-
         self.assertEqual(response.status_code, 200)
         self.assertEqual(new_tag.name, tag_attributes["name"])
 
+
+class TagShowViewTest(LoggedInTestMixin, TestCase):
+
     def test_tag_show_view(self):
-        user = UserFactory()
-        tag = TagFactory(owner=user)
-        self.client.login(username=user.username, password="tester")
+        tag = TagFactory(owner=self.user)
         url = reverse("tags:tag_show", args=[tag.id])
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(tag, response.context['object'])
 
+
+class TagEditViewTest(LoggedInTestMixin, TestCase):
+
     def test_tag_edit_view(self):
-        user = UserFactory()
-        tag = TagFactory(owner=user)
-        self.client.login(username=user.username, password="tester")
+        tag = TagFactory(owner=self.user)
         url = reverse("tags:tag_edit", args=[tag.id])
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.context["form"], TagForm)
 
+
+class TagDeleteViewTest(LoggedInTestMixin, TestCase):
+
     def test_tag_delete_view(self):
-        user = UserFactory()
-        tag = TagFactory(owner=user)
-        self.client.login(username=user.username, password="tester")
+        tag = TagFactory(owner=self.user)
         url = reverse("tags:tag_delete", args=[tag.id])
         response = self.client.get(url)
 
