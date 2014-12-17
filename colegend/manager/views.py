7dff3bb08072193e28c28a203b752159265e369a
Eraldo Helal <eraldo@eraldo.org>
2014-12-17 05:02:16 +0100

    Redesigned Agenda to show more and use less vertical space.
    [skip ci]

--- colegend/manager/views.py
+++ colegend/manager/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.views.generic import TemplateView
 from lib.views import ActiveUserRequiredMixin
 
@@ -11,6 +12,7 @@ class AgendaView(ActiveUserRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         user = self.request.user
         context["top_projects"] = user.projects.open()[:3]
-        context["top_deadline_tasks"] = user.tasks.open().filter(deadline__isnull=False).order_by('deadline')[:4]
-        context["single_tasks"] = user.tasks.open().filter(project__isnull=True)[:4]
+        context["top_scheduled_tasks"] = user.tasks.open().filter(date=timezone.now())[:2]
+        context["top_deadlined_tasks"] = user.tasks.open().filter(deadline__isnull=False).order_by('deadline')[:2]
+        context["top_single_tasks"] = user.tasks.open().filter(project__isnull=True)[:7]
         return context
