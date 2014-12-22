49da87812ef7cacec37bd7f35683fa8095151e9e
Eraldo Helal <eraldo@eraldo.org>
2014-12-22 01:59:35 +0100

    Added pager to feature list.
    [skip ci]

--- colegend/features/views.py
+++ colegend/features/views.py
@@ -8,6 +8,7 @@ class FeatureListView(ListView):
     model = Feature
     fields = ['name', 'description', 'date_published']
     icon = "feature"
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
