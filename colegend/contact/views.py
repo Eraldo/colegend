99ffee3d4d211d5a49144ee2f498a2c7b8243615
Eraldo Helal <eraldo@eraldo.org>
2015-02-11 02:59:40 +0100

    Added redirection to next url to contact form.
    [skip ci]

--- colegend/contact/views.py
+++ colegend/contact/views.py
@@ -38,6 +38,12 @@ class ContactView(FormView):
         context['eraldo'] = eraldo
         return context
 
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+        return super().get_success_url()
+
 
 class MessageView(ActiveUserRequiredMixin, FormView):
     template_name = "contact/message.html"
