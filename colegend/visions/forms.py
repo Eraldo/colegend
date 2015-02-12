b82ff6bff85e535bcdabbdb1cbda524418c8d814
Eraldo Helal <eraldo@eraldo.org>
2015-02-12 01:08:58 +0100

    Added first version of content checks to life vision.
    [skip ci]

--- colegend/visions/forms.py
+++ colegend/visions/forms.py
@@ -1,5 +1,6 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
+from django import forms
 from django.forms import ModelForm
 from markitup.widgets import MarkItUpWidget
 from visions.models import Vision
@@ -8,6 +9,15 @@ __author__ = 'eraldo'
 
 
 class VisionForm(ModelForm):
+    you = forms.BooleanField(
+        help_text="The vision is about you. What you do and what you want.",
+        initial=False
+    )
+    present = forms.BooleanField(
+        help_text="Your vision is formulated in the present tense: 'I live in ...'",
+        initial=False
+    )
+
     class Meta:
         model = Vision
         fields = ['name', 'content']
@@ -19,5 +29,7 @@ class VisionForm(ModelForm):
     helper.layout = Layout(
         Field('name', autofocus='True'),
         Field('content', rows="20"),
+        Field('you'),
+        Field('present'),
     )
     helper.add_input(Submit('save', 'Save'))
