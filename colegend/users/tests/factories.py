72d21cb27a4342238776eb0d8b5992bb17af91e1
Eraldo Energy <eraldo@eraldo.org>
2015-11-16 11:45:18 +0100

    Added user factory.

---
+++ colegend/users/tests/factories.py
@@ -0,0 +1,13 @@
+import factory
+
+from colegend.users.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Sequence(lambda n: 'user-{0}'.format(n))
+    email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n))
+    password = factory.PostGenerationMethodCall('set_password', 'password')
+
+    class Meta:
+        model = User
+        django_get_or_create = ('username', )
