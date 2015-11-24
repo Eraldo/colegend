3051ff469bea217b14ab94d9796ac35a3ce1820d
Eraldo Energy <eraldo@eraldo.org>
2015-11-24 23:34:22 +0100

    Restructured chapter 1 base.
    Added a template for chats.
    Added a guidelines introduction chat.
    Added a virtual room view and link to it.
    
    [skip ci]

--- connected/urls.py
+++ connected/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url
-
-from .views import ConnectedView
+from .views import ConnectedView, VirtualRoomView, GuidelinesView
 
 __author__ = 'eraldo'
 
@@ -10,4 +9,10 @@ urlpatterns = patterns(
     url(r'^$',
         ConnectedView.as_view(),
         name='index'),
+    url(r'^virtual-room/$',
+        VirtualRoomView.as_view(),
+        name='virtual-room'),
+    url(r'^guidelines/$',
+        GuidelinesView.as_view(),
+        name='guidelines'),
 )
--- connected/views.py
+++ connected/views.py
@@ -1,8 +1,17 @@
 from braces.views import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 __author__ = 'Eraldo Energy'
 
 
 class ConnectedView(LoginRequiredMixin, TemplateView):
     template_name = 'connected/connected.html'
+
+
+class GuidelinesView(LoginRequiredMixin, TemplateView):
+    template_name = 'connected/guidelines.html'
+
+
+class VirtualRoomView(LoginRequiredMixin, RedirectView):
+    permanent = False
+    url = 'https://plus.google.com/hangouts/_/colegend42g3psmu3okj5ym27qa'
