195224576d5c88c6e9e8e20322841c0a36587837
Eraldo Energy <eraldo@eraldo.org>
2015-11-29 00:43:55 +0100

    Updated test settings.

--- config/settings/test.py
+++ config/settings/test.py
@@ -11,6 +11,3 @@ from .local import *  # noqa
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
-
-# Used to find base.test template to extend it.
-INSTALLED_APPS += ('tests',)
