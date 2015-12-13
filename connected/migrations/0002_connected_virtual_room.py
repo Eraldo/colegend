f7e555df1dda184de1f8e0b9a4d56b916c7fd5a8
Eraldo Energy <eraldo@eraldo.org>
2015-12-13 22:44:20 +0100

    Added virtual room check to connected path.

---
+++ connected/migrations/0002_connected_virtual_room.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('connected', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='connected',
+            name='virtual_room',
+            field=models.BooleanField(default=False),
+        ),
+    ]
