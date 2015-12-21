2eebe742abd34fe099c8b486a5524c5cc0cee6dd
Eraldo Energy <eraldo@eraldo.org>
2015-12-22 00:09:23 +0100

    Added moved story files.

---
+++ story/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, url
+from .views import Chapter1View, PrologueView, WelcomeTreeView, TreeMessageWidgetView
+
+__author__ = 'eraldo'
+
+urlpatterns = patterns(
+    '',
+    url(r'^chapter-1$',
+        Chapter1View.as_view(),
+        name='chapter1'),
+    url(r'^welcome-tree$',
+        WelcomeTreeView.as_view(),
+        name='welcome-tree'),
+    url(r'^prologue$',
+        PrologueView.as_view(),
+        name='prologue'),
+    url(r'^welcome-tree/message$',
+        TreeMessageWidgetView.as_view(),
+        name='tree-message'),
+)
