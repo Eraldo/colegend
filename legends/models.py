8bdf49c9f4b4d3e7a6b7801756531eb6cea9fc90
Eraldo Energy <eraldo@eraldo.org>
2015-12-29 17:48:47 +0100

    Added a dynamic media upload path based on a combination of user and model name.

--- legends/models.py
+++ legends/models.py
@@ -1,7 +1,27 @@
+import os
+
 from django.core.urlresolvers import reverse
 from django.db import models
 
 from core.models import AutoOwnedBase, TimeStampedBase
+
+
+def owner_path(sub_directory=None, attribute='owner'):
+    # Returns a function that will generate a user path with an optional sub-directory.
+    if sub_directory:
+        template = 'user_{{0}}/{}/{{1}}'.format(sub_directory)
+    else:
+        template = 'user_{0}/{1}'
+
+    def specific_user_path(instance, filename):
+        # file will be uploaded to MEDIA_ROOT/user_<id>/(?:<sub_directory>/)<filename>
+        owner = getattr(instance, attribute)
+        return template.format(owner.id, filename)
+
+    return specific_user_path
+
+
+legend_path = owner_path('legend')
 
 
 class Legend(AutoOwnedBase, TimeStampedBase):
@@ -11,7 +31,7 @@ class Legend(AutoOwnedBase, TimeStampedBase):
     + hero profile
     + demon profile
     """
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to=legend_path, blank=True)
 
     def __str__(self):
         return "Legend {}".format(self.owner)
