d07407c3123bcd7a98933ddb4d7cbb9fad429bda
Eraldo Energy <eraldo@eraldo.org>
2015-12-31 14:40:53 +0100

    Added media path factory.

---
+++ core/utils/media_paths.py
@@ -0,0 +1,31 @@
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class UploadToOwnedDirectory(object):
+    # Returns a factory that will generate a user media upload path with an optional sub-directory.
+    template = "users/{id}_{username}/{sub_directory}{filename}"
+    user_attribute = 'owner'
+    sub_directory = ''
+
+    def __init__(self, sub_directory=None, user_attribute=None):
+        if sub_directory:
+            if not sub_directory.endswith('/'):
+                sub_directory += '/'
+            self.sub_directory = sub_directory
+        if user_attribute:
+            self.user_attribute = user_attribute
+
+    def __call__(self, instance, filename):
+        # file will be uploaded to MEDIA_ROOT/users/<id>_<username>/(?:<sub_directory>/)<filename>
+        user_attribute = self.user_attribute
+
+        owner = getattr(instance, user_attribute)
+        id = owner.id
+        username = owner.username
+
+        sub_directory = self.sub_directory
+
+        template = self.template
+
+        return template.format(id=id, username=username, sub_directory=sub_directory, filename=filename)
