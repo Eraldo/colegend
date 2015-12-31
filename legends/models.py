810ab06289dd4199bf276cdd4a489689a2aa3fc3
Eraldo Energy <eraldo@eraldo.org>
2015-12-31 14:41:43 +0100

    Added legend avatar to main menu and legend detail page.

--- legends/models.py
+++ legends/models.py
@@ -1,27 +1,8 @@
-import os
-
 from django.core.urlresolvers import reverse
-from django.db import models
+from easy_thumbnails.fields import ThumbnailerImageField
 
 from core.models import AutoOwnedBase, TimeStampedBase
-
-
-def owner_path(sub_directory=None, attribute='owner'):
-    # Returns a function that will generate a user path with an optional sub-directory.
-    if sub_directory:
-        template = 'user_{{0}}/{}/{{1}}'.format(sub_directory)
-    else:
-        template = 'user_{0}/{1}'
-
-    def specific_user_path(instance, filename):
-        # file will be uploaded to MEDIA_ROOT/user_<id>/(?:<sub_directory>/)<filename>
-        owner = getattr(instance, attribute)
-        return template.format(owner.id, filename)
-
-    return specific_user_path
-
-
-legend_path = owner_path('legend')
+from core.utils.media_paths import UploadToOwnedDirectory
 
 
 class Legend(AutoOwnedBase, TimeStampedBase):
@@ -31,7 +12,7 @@ class Legend(AutoOwnedBase, TimeStampedBase):
     + hero profile
     + demon profile
     """
-    avatar = models.ImageField(upload_to=legend_path, blank=True)
+    avatar = ThumbnailerImageField(upload_to=UploadToOwnedDirectory('legend'), blank=True)
 
     def __str__(self):
         return "Legend {}".format(self.owner)
