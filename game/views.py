84286fe8313ef4e8dff0fda5784a64d381732b7d
Eraldo Energy <eraldo@eraldo.org>
2016-01-09 03:32:12 +0100

    Only show cards in hand on game index page.

--- game/views.py
+++ game/views.py
@@ -8,27 +8,16 @@ class GameIndexView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        cards_data = [
-            {
-                'title': 'card 1',
-                'content': 'Some text.',
-                'source': 'http://lorempixel.com/400/200/cats/card/',
-            },
-            {
-                'title': 'card 2 - blub',
-                'content': 'Some more text.',
-                'source': 'http://lorempixel.com/400/200/cats/card/',
-            },
-            {
-                'title': 'card 2',
-                'content': 'Some even longer text. Some even longer text. Some even longer text. Some even longer text.',
-                'source': 'http://lorempixel.com/400/200/cats/card/',
-            },
-        ]
-        cards = []
-        template = 'game/widgets/card.html'
-        for data in cards_data:
-            card = render_to_string(template, data)
-            cards.append(card)
-        context['cards'] = cards
+        user = self.request.user
+        rendered_cards = []
+        card_template = 'game/widgets/card.html'
+        for card in user.game.hand.all():
+            card_context = {
+                'title': card.name,
+                'content': card.content,
+                'source': card.image.url if card.image else '',
+            }
+            rendered_card = render_to_string(card_template, card_context)
+            rendered_cards.append(rendered_card)
+        context['cards'] = rendered_cards
         return context
