bb78cd4a81e607f6a571a65f3f01b5ed7c716880
Eraldo Energy <eraldo@eraldo.org>
2016-01-09 02:32:24 +0100

    Added game and checkpoint models to game app.

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from orderable.admin import OrderableAdmin
-from .models import Card
+from .models import Card, Checkpoint, Game
 
 
 @admin.register(Card)
 class CardAdmin(OrderableAdmin):
     pass
+
+
+@admin.register(Checkpoint)
+class CheckpointAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    filter_horizontal = ['hand', 'completed', 'checkpoints']
--- game/models.py
+++ game/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from orderable.models import Orderable
 from categories.models import Category
+from core.models import AutoOwnedBase
 
 
 class Card(Orderable):
@@ -8,7 +9,27 @@ class Card(Orderable):
     name = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     details = models.TextField(blank=True)
-    category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category, related_name="cards")
 
     def __str__(self):
         return self.name
+
+
+class Checkpoint(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(AutoOwnedBase):
+    hand = models.ManyToManyField(Card, related_name='games_hand', blank=True)
+    completed = models.ManyToManyField(Card, related_name='games_completed', blank=True)
+    checkpoints = models.ManyToManyField(Checkpoint, blank=True)
+
+    @property
+    def deck(self):
+        return Card.objects.exclude(id__in=[self.completed, self.hand])
+
+    def __str__(self):
+        return "{}'s game".format(self.owner)
