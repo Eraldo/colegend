c5371e175d9c3c69c2e941cc8ce7250a52e12e49
Eraldo Energy <eraldo@eraldo.org>
2016-01-11 12:05:40 +0100

    Cleanup.

--- cards/models.py
+++ cards/models.py
@@ -1,6 +1,5 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.template.defaultfilters import slugify
 from orderable.models import Orderable
 from categories.models import Category
 
