38b61f4061262aa308c7d3c0cc06ffc90caa311d
Eraldo Energy <eraldo@eraldo.org>
2016-01-17 14:03:39 +0100

    Added role model base.

--- roles/admin.py
+++ roles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Role
 
-# Register your models here.
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
--- roles/models.py
+++ roles/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import Permission
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(_('name'), max_length=255)
