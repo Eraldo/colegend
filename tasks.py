58c9854f6face1752f761921cbb5d8807d39ffd2
Eraldo Energy <eraldo@eraldo.org>
2016-01-17 03:18:19 +0100

    Updated maintenance tasks.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 import os
 from subprocess import Popen, PIPE
 import signal
+from time import sleep
 
 from celery.utils.term import colored
 from invoke import run as invoke_run, task
@@ -8,23 +9,49 @@ from invoke import run as invoke_run, task
 project_name = 'colegend'
 context = {
     'environment': 'development',
+    'branch': 'development',
 }
 
 
 def message(text, *args, **kwargs):
+    """
+    Displaying a message for the user.
+    """
     environment = context.get('environment', '---')
     c = colored(enabled=True)
     print(c.cyan('\n>> [{}] {}'.format(environment, text)), *args, **kwargs)
 
 
-def run(command, *args, **kwargs):
+def local_run(command):
     print('$: {}'.format(command))
+    invoke_run(command)
+
+
+def heroku_run(command='run', parameters=''):
+    """
+    Running a command for the chosen environment on heroku.
+    """
+    environment = context.get('environment')
+    app = project_name
+    if environment == 'staging':
+        app += '-staging'
+    local_run('heroku {} --app {} {}'.format(command, app, parameters))
+
+
+def run(command, heroku_command='run', *args, **kwargs):
+    """
+    Running a command for the chosen environment.
+    """
     if context.get('environment') == 'development':
-        return invoke_run('source ~/.virtualenvs/{}/bin/activate && {}'.format(project_name, command))
-    elif context.get('environment') == 'staging':
-        return invoke_run('heroku run --app {}-staging {}'.format(project_name, command), *args, **kwargs)
-    elif context.get('environment') == 'production':
-        return invoke_run('heroku run --app {} {}'.format(project_name, command), *args, **kwargs)
+        return local_run('source ~/.virtualenvs/{}/bin/activate && {}'.format(project_name, command))
+    elif context.get('environment') in ['staging', 'production']:
+        return heroku_run(heroku_command, parameters=command, *args, **kwargs)
+
+
+def git_push():
+    branch = context.get('branch')
+    command = 'git push origin {}'.format(branch)
+    local_run(command)
 
 
 @task
@@ -33,6 +60,7 @@ def development():
     Setting up development environment. (default)
     """
     context['environment'] = 'development'
+    context['branch'] = 'development'
 
 
 @task
@@ -41,6 +69,7 @@ def staging():
     Setting up staging environment.
     """
     context['environment'] = 'staging'
+    context['branch'] = 'staging'
 
 
 @task
@@ -49,6 +78,7 @@ def production():
     Setting up production environment.
     """
     context['environment'] = 'production'
+    context['branch'] = 'master'
 
 
 @task
@@ -69,10 +99,37 @@ def migrate():
 
 
 @task
+def deploy():
+    """
+    Delopy the app to heroku.
+    """
+    if context.get('environment') in ['staging', 'production']:
+        message('deploying the app')
+        maintenance(on=True)
+        git_push()
+        sleep(30)
+        migrate()
+        maintenance(off=True)
+
+
+@task
+def maintenance(on=False, off=False):
+    """
+    Start or stop maintenance:on mode.
+    """
+    if context.get('environment') in ['staging', 'production']:
+        if on:
+            message('starting maintenance:on')
+            run('', heroku_command='maintenance:on')
+        elif off:
+            message('stopping maintenance')
+            run('', heroku_command='maintenance:off')
+
+
+@task
 def backup():
     """
     Backup the database. (development only)
-    :return:
     """
     if context.get('environment') == 'development':
         message('backing up database')
