7aa90d8a25a656ae1f90551e41952d2ae1ac529e
Eraldo Energy <eraldo@eraldo.org>
2016-01-19 18:37:19 +0100

    Removed chapter progress from continuous view.

--- continuous/views.py
+++ continuous/views.py
@@ -1,4 +1,3 @@
-from annoying.functions import get_object_or_None
 from braces.views import LoginRequiredMixin
 from django.views.generic import TemplateView
 
@@ -7,31 +6,3 @@ __author__ = 'Eraldo Energy'
 
 class ContinuousView(LoginRequiredMixin, TemplateView):
     template_name = 'continuous/continuous.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
-        if user.continuous.chapter == 1:
-            progress = self.get_chapter_1_progress()
-        else:
-            progress = '4'
-        context['progress'] = progress
-
-        return context
-
-    def get_chapter_1_progress(self):
-        user = self.request.user
-        completed = 0
-        steps = [
-            hasattr(user, 'outercall'),
-            hasattr(user, 'innercall'),
-            user.profile.biography,
-            user.connected.guidelines,
-            user.connected.chat,
-            user.connected.guide,
-            # TODO: story chapter 1 steps
-        ]
-        for step in steps:
-            if step:
-                completed += 1
-        return int(completed / len(steps) * 100)
