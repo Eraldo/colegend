882e8bc90177b198a807810df8f65176f3b6e0a6
Eraldo Energy <eraldo@eraldo.org>
2016-01-26 11:49:18 +0100

    Updated journal queryset.

--- colegend/journals/models.py
+++ colegend/journals/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from colegend.core.models import AutoOwnedBase, AutoUrlsMixin, OwnedQuerySet
 
 
-class JournalQuerySet(OwnedQuerySet, models.QuerySet):
+class JournalQuerySet(OwnedQuerySet):
     pass
 
 
