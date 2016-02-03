2c285ece3c72b622b7b7a68fdc6335388882e362
Eraldo Energy <eraldo@eraldo.org>
2016-02-04 00:04:55 +0100

    Added tag tempalte to tags list.

--- colegend/tags/templatetags/tags_tags.py
+++ colegend/tags/templatetags/tags_tags.py
@@ -14,3 +14,15 @@ def tag_link(context, tag=None, **kwargs):
     context.update(kwargs)
     template = 'tags/widgets/link.html'
     return render_to_string(template, context=context)
+
+
+@register.simple_tag(takes_context=True)
+def tag(context, tag=None, **kwargs):
+    tag = tag or context.get('tag')
+    context = {
+        'name': tag,
+        'url': tag.get_absolute_url(),
+    }
+    context.update(kwargs)
+    template = 'tags/widgets/tag.html'
+    return render_to_string(template, context=context)
