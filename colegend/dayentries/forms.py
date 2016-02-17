34b9d28f9a63f701a84fa5bf326e3ef0dbeba43a
Eraldo Energy <eraldo@eraldo.org>
2016-02-17 17:22:08 +0100

    Refactored project fonts.

--- colegend/dayentries/forms.py
+++ colegend/dayentries/forms.py
@@ -26,7 +26,7 @@ class DayEntryForm(forms.ModelForm):
 
         # Update the tags field to use the custom django-autocomplete's create field
         tags_queryset = self.fields.get('tags').queryset
-        self.fields['tags'] = TagsCreateFormField(tags_queryset, owner)
+        self.fields['tags'] = TagsCreateFormField(tags_queryset, owner, required=False)
 
         self.helper = FormHelper()
         self.helper.layout = Layout(
