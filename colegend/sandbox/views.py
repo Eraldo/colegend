207b81e8a21471d727de7598ec321aa1d162d33d
Eraldo Energy <eraldo@eraldo.org>
2016-05-07 18:00:19 +0200

    Played in sandbox.

--- colegend/sandbox/views.py
+++ colegend/sandbox/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
-from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+
+from colegend.cards.models import Card
 
 
 class SandboxView(TemplateView):
@@ -12,6 +13,7 @@ class SandboxView(TemplateView):
         messages.success(request, "Test page loaded")
         return super().get(request, *args, **kwargs)
 
-        # def get_context_data(self, **kwargs):
-        #     context = super().get_context_data(**kwargs)
-        #     context['user'] = self.request.u
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['card'] = Card.objects.first()
+        return context
