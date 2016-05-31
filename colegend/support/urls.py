a014ec7eeddcb97fa502e90d1de8097648c4a4c5
Eraldo Energy <eraldo@eraldo.org>
2016-06-01 01:39:52 +0200

    Fixed namespace issues in cms.
    Using app_name in the urls.py does not seem to work yet with cms apphooks.
    I got an error complaining that the namespace is not registered.

--- colegend/support/urls.py
+++ colegend/support/urls.py
@@ -3,7 +3,6 @@ from .views import DocumentationView, FAQView, SupportView
 
 __author__ = 'eraldo'
 
-app_name = 'support'
 urlpatterns = [
     url(r'^$',
         SupportView.as_view(),
@@ -14,4 +13,7 @@ urlpatterns = [
     url(r'^documentation/$',
         DocumentationView.as_view(),
         name='documentation'),
+]
+urlpatterns = [
+    url(r'^', include(urlpatterns, namespace='support')),
 ]
