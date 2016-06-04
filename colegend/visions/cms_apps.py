c4de83320efc42ac249eb4e12e1eafcf82acf65a
Eraldo Energy <eraldo@eraldo.org>
2016-06-04 21:06:00 +0200

    Updated vision app.

--- colegend/visions/cms_apps.py
+++ colegend/visions/cms_apps.py
@@ -2,13 +2,10 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-# from .cms_menus import VisonMenu
-
 
 class VisionsApphook(CMSApp):
     name = _('Visions')
     urls = ['colegend.visions.urls']
-    # menus = [VisonMenu]
 
 
 apphook_pool.register(VisionsApphook)
