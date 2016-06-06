001a6c11c768cb2e753b677a9d1b455c103af730
Eraldo Energy <eraldo@eraldo.org>
2016-06-06 12:32:26 +0200

    Updated the legend widget.

--- colegend/core/templatetags/core_tags.py
+++ colegend/core/templatetags/core_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.core.exceptions import ValidationError
+from django.template.loader import render_to_string
 
 from colegend.core.intuitive_duration.utils import intuitive_duration_string
 
@@ -12,3 +13,12 @@ def intuitive_duration(value):
         return intuitive_duration_string(value)
     except ValidationError:
         return ''
+
+
+@register.simple_tag(takes_context=True)
+def label(context, label=None, **kwargs):
+    label = label or context.get('label', {})
+    label_context = label
+    label_context.update(kwargs)
+    label_template = 'widgets/label.html'
+    return render_to_string(label_template, context=label_context)
--- colegend/users/templatetags/legends_tags.py
+++ colegend/users/templatetags/legends_tags.py
@@ -1,11 +1,13 @@
 from django import template
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 
 register = template.Library()
 
 
+# Future: Refactor this tag to a shortcut reusing the other tags.
 @register.simple_tag(takes_context=True)
 def legend(context, legend=None, size=None, **kwargs):
     # if no legend is given take the legend from the context or else the user
@@ -29,6 +31,53 @@ def legend(context, legend=None, size=None, **kwargs):
             context['source'] = legend.get_avatar(size=size).url
         template = 'legends/widgets/legend.html'
     return render_to_string(template, context=context)
+
+
+@register.simple_tag(takes_context=True)
+def legend_avatar(context, legend=None, size=None, **kwargs):
+    # if no legend is given take the legend from the context or else the user
+    legend = legend or context.get('legend', context.get('user'))
+    size = size or context.get('size', 'medium')
+    legend_context = {
+        'label': {
+            'class': 'label-pill bg-accent',
+        },
+        'size': size,
+    }
+    if legend and legend.is_authenticated():
+        legend_context.update({
+            'name': legend,
+            'image': legend.get_avatar(size=size).url if legend.avatar else static('legends/images/anonymous.png'),
+            'url': legend.get_absolute_url(),
+        })
+    else:
+        legend_context.update({
+            'name': 'Anonymous',
+            'image': static('legends/images/anonymous.png'),
+        })
+    legend_context.update(kwargs)
+    template = 'legends/widgets/avatar.html'
+    return render_to_string(template, context=legend_context)
+
+
+@register.simple_tag(takes_context=True)
+def legend_link(context, legend=None, size=None, **kwargs):
+    # if no legend is given take the legend from the context or else the user
+    legend = legend or context.get('legend', context.get('user'))
+    legend_context = {}
+    if legend and legend.is_authenticated():
+        legend_context.update({
+            'content': legend,
+            'url': legend.get_absolute_url(),
+        })
+    else:
+        legend_context.update({
+            'content': 'Anonymous',
+            'url': reverse('join'),
+        })
+    legend_context.update(kwargs)
+    template = 'widgets/link.html'
+    return render_to_string(template, context=legend_context)
 
 
 @register.simple_tag()
