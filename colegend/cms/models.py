a24f31f3cf2b53e90b390bc79213e6534bc41f32
Eraldo Energy <eraldo@eraldo.org>
2016-06-20 14:02:08 +0200

    Fixed cms single page mixin migration error.

--- colegend/cms/models.py
+++ colegend/cms/models.py
@@ -17,7 +17,7 @@ class SinglePageMixin(object):
     def clean_parent_page_models(cls):
         # Only allow a single instance.
         try:
-            if cls.objects.first().exists():
+            if cls.objects.exists():
                 return []
         except ProgrammingError:  # not migrated yet.
             pass
