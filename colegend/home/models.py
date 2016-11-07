af762069d08b8061d311019a597186f0893ad375
Eraldo Energy <eraldo@eraldo.org>
2016-11-07 22:13:16 +0100

    Added next step to home view.

--- colegend/home/models.py
+++ colegend/home/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils import timezone
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
 
 from colegend.cms.models import UniquePageMixin
+from colegend.core.templatetags.core_tags import link
+
+from django.utils.translation import ugettext_lazy as _
 
 
 class HomePage(UniquePageMixin, Page):
@@ -27,6 +32,18 @@ class HomePage(UniquePageMixin, Page):
         if not user.has_checkpoint('prologue'):
             return redirect("story:prologue")
         return super().serve(request, *args, **kwargs)
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context['next_step'] = self.get_next_step(request.user)
+        return context
+
+    def get_next_step(self, user):
+        # Has the user written his journal entry?
+        today = timezone.now().date()
+        dayentry = user.journal.dayentries.filter(date=today)
+        if not dayentry:
+            return link(_('Create a journal entry'), reverse('dayentries:create'))
 
     def __str__(self):
         return self.title
