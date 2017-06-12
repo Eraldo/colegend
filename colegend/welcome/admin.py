cfcf3173221004f127f7be8f8caebbca12e2361c
Eraldo Energy <eraldo@eraldo.org>
2017-06-12 19:57:43 +0200

    Added join waiting list.

--- colegend/welcome/admin.py
+++ colegend/welcome/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from colegend.welcome.models import WaitingUser
+
+
+@admin.register(WaitingUser)
+class WaitingUserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'created', 'informed', 'accepted']
+    readonly_fields = ['created', 'modified']
--- colegend/welcome/models.py
+++ colegend/welcome/models.py
@@ -1,15 +1,35 @@
+from random import randint
+
 from allauth.account.utils import perform_login
 from django.conf import settings
 from django.contrib import messages
+from django.db import IntegrityError
 from django.shortcuts import redirect
+from django.template.response import TemplateResponse
 from django.urls import reverse
+from django.db import models
+from wagtail.contrib.wagtailroutablepage.models import RoutablePageMixin, route
 from wagtail.wagtailcore.models import Page
 
+from colegend.core.models import TimeStampedBase
 from colegend.users.models import User
 from colegend.welcome.forms import WelcomeEmailForm, WelcomePasswordForm
 
 
-class WelcomePage(Page):
+def generate_code():
+    return randint(1000, 9999)
+
+
+class WaitingUser(TimeStampedBase, models.Model):
+    email = models.EmailField(
+        unique=True
+    )
+    code = models.PositiveSmallIntegerField(default=generate_code)
+    informed = models.BooleanField(default=False)
+    accepted = models.BooleanField(default=False)
+
+
+class WelcomePage(RoutablePageMixin, Page):
     template = 'welcome/base.html'
 
     parent_page_types = ['cms.RootPage']
@@ -20,7 +40,8 @@ class WelcomePage(Page):
     password = ''
     user = None
 
-    def serve(self, request, *args, **kwargs):
+    @route(r'^$')
+    def index(self, request, *args, **kwargs):
         email = request.POST.get('email')
         if email:
             self.email = email
@@ -39,8 +60,13 @@ class WelcomePage(Page):
                     else:
                         messages.error(request, 'Something did not match. Please try again!')
             else:
-                signup_url = reverse('account_signup') + '?email=' + email
-                return redirect(signup_url)
+                # Adding email to waiting list.
+                try:
+                    WaitingUser.objects.create(email=email)
+                except IntegrityError:
+                    pass
+                next_url = self.url + self.reverse_subpage('waiting_list') + '?email={0}'.format(email)
+                return redirect(next_url)
         return super().serve(request, *args, **kwargs)
 
     def get_context(self, request, *args, **kwargs):
@@ -57,3 +83,26 @@ class WelcomePage(Page):
                 else:
                     context['form'] = WelcomePasswordForm(initial=data)
         return context
+
+    @route(r'^waiting-list/$')
+    def waiting_list(self, request):
+        email = request.GET.get('email') or request.POST.get('email')
+        code = request.POST.get('code')
+        if email and code:
+            found = False
+            try:
+                found = WaitingUser.objects.get(email=email, code=int(code))
+            except WaitingUser.DoesNotExist:
+                pass
+            if found:
+                found.accepted = True
+                found.save()
+                next_url = reverse('account_signup') + '?email=' + email
+                return redirect(next_url)
+            else:
+                messages.error(request, 'Attempt failed. Please make sure you use the correct code.')
+        return TemplateResponse(
+            request,
+            'welcome/waiting_list.html',
+            {'email': email}
+        )
