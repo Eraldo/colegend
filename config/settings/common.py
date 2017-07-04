b356b793323d5234590496816696e13e055a2146
Eraldo Energy <eraldo@eraldo.org>
2017-07-04 15:32:47 +0200

    Disabling session login as workaround.

--- config/settings/common.py
+++ config/settings/common.py
@@ -393,7 +393,7 @@ TAGGIT_CASE_INSENSITIVE = True
 # ------------------------------------------------------------------------------
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
