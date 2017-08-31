f534ba6a9ef318ff2e2bced5b50560ea95d8ca3e
Eraldo Energy <eraldo@eraldo.org>
2017-08-31 18:23:09 +0200

    Added current user and level to user schema.

--- colegend/users/schema.py
+++ colegend/users/schema.py
@@ -13,6 +13,11 @@ def convert_phone_number_to_string(field, registry=None):
 
 
 class UserType(DjangoObjectType):
+    level = graphene.Field(
+        graphene.Int,
+        app=graphene.Int(),
+    )
+
     class Meta:
         model = User
         filter_fields = {
@@ -20,20 +25,41 @@ class UserType(DjangoObjectType):
         }
         interfaces = [graphene.Node]
 
+    def resolve_level(self, info):
+        user = info.context.user
+        if user.is_authenticated:
+            return user.experience.level()
+        return 0
+
 
 class Query(graphene.ObjectType):
+    my_user = graphene.Field(
+        UserType
+    )
     user = graphene.Node.Field(UserType)
     users = DjangoFilterConnectionField(UserType)
+
+    def resolve_my_user(self, info):
+        return info.context.user
+
+    # def resolve_my_posts(self, args, context, info):
+    #     # context will reference to the Django request
+    #     if not context.user.is_authenticated():
+    #         return Post.objects.none()
+    #     else:
+    #         return Post.objects.filter(owner=context.user)
+
     # user = graphene.Field(
     #     UserType,
     #     id=graphene.Int(),
     #     name=graphene.String(),
     # )
+
     # users = graphene.List(UserType)
-    #
+
     # def resolve_users(self, info):
     #     return User.objects.all()
-    #
+
     # def resolve_user(self, info, id=None, name=None):
     #     if id is not None:
     #         return User.objects.get(id=id)
