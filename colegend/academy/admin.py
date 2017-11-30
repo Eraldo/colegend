22a0ce50ca09909a7100a6620ad3c5c4ed3c6241
Eraldo Energy <eraldo@eraldo.org>
2017-11-30 21:29:15 +0100

    Added book and book review models.

--- colegend/academy/admin.py
+++ colegend/academy/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book, BookReview
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author', 'featured']
+    list_filter = ['featured']
+    readonly_fields = ['created']
+
+
+@admin.register(BookReview)
+class BookReviewAdmin(admin.ModelAdmin):
+    list_display = ['book', 'owner']
+    list_filter = ['owner', 'book']
+    readonly_fields = ['created']
--- colegend/academy/models.py
+++ colegend/academy/models.py
@@ -1,5 +1,103 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
 from django.shortcuts import redirect
 from wagtail.wagtailcore.models import Page
+
+from colegend.core.fields import MarkdownField
+from colegend.core.models import TimeStampedBase, OwnedBase
+from django.utils.translation import ugettext_lazy as _
+
+
+class Book(TimeStampedBase):
+    name = models.CharField(
+        _('name'),
+        max_length=255,
+        unique=True
+    )
+    author = models.CharField(
+        _('author'),
+        max_length=255,
+    )
+    image_url = models.URLField(
+        _('image url'),
+        blank=True
+    )
+    url = models.URLField(
+        _('url'),
+        blank=True
+    )
+    content = MarkdownField(
+        blank=True
+    )
+    featured = models.BooleanField(
+        default=False
+    )
+
+    class Meta:
+        default_related_name = 'books'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        # Making sure only one book can be featured.
+
+        if self.featured:
+            try:
+                temp = Book.objects.get(featured=True)
+                if self != temp:
+                    temp.featured = False
+                    temp.save()
+            except Book.DoesNotExist:
+                pass
+        return super().save(*args, **kwargs)
+
+
+class BookReview(OwnedBase, TimeStampedBase):
+    book = models.ForeignKey(
+        to=Book,
+        on_delete=models.CASCADE
+    )
+    rating = models.PositiveSmallIntegerField(
+        _('rating'),
+    )
+    area_1 = models.PositiveSmallIntegerField(
+        _('area 1'),
+        validators=[MaxValueValidator(100)]
+    )
+    area_2 = models.PositiveSmallIntegerField(
+        _('area 2'),
+        validators=[MaxValueValidator(100)]
+    )
+    area_3 = models.PositiveSmallIntegerField(
+        _('area 3'),
+        validators=[MaxValueValidator(100)]
+    )
+    area_4 = models.PositiveSmallIntegerField(
+        _('area 4'),
+        validators=[MaxValueValidator(100)]
+    )
+    area_5 = models.PositiveSmallIntegerField(
+        _('area 5'),
+        validators=[MaxValueValidator(100)]
+    )
+    area_6 = models.PositiveSmallIntegerField(
+        _('area 6'),
+        validators=[MaxValueValidator(100)]
+    )
+    area_7 = models.PositiveSmallIntegerField(
+        _('area 7'),
+        validators=[MaxValueValidator(100)]
+    )
+    content = MarkdownField()
+
+    class Meta:
+        default_related_name = 'book_reviews'
+        unique_together = ['owner', 'book']
+
+    def __str__(self):
+        return 'Book review'
 
 
 class AcademyPage(Page):
@@ -52,7 +150,6 @@ class QuizzesPage(Page):
 
     def __str__(self):
         return self.title
-
 
 # class ResourcesPage(Page):
 #     template = 'academy/resources.html'
