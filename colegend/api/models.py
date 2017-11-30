3626f71e71ef14ad4de32d4889f6d21219f009fe
Eraldo Energy <eraldo@eraldo.org>
2017-11-30 20:02:22 +0100

    Added an abstract countable connection class.

--- colegend/api/models.py
+++ colegend/api/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from graphene import relay, Int
 
-# Create your models here.
+
+class CountableConnectionBase(relay.Connection):
+    class Meta:
+        abstract = True
+
+    total_count = Int()
+
+    def resolve_total_count(self, info, **kwargs):
+        return self.iterable.count()
