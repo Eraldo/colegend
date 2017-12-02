39b8f7466c7ee09a3b702f1974b58a9e01f3f5c0
Eraldo Energy <eraldo@eraldo.org>
2017-12-02 21:54:08 +0100

    Added review inlines to book admin.

--- colegend/academy/admin.py
+++ colegend/academy/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from .models import Book, BookReview
 
 
+class BookReviewInline(admin.TabularInline):
+    fields = ['owner', 'rating', 'area_1', 'area_2', 'area_3', 'area_4', 'area_5', 'area_6', 'area_7']
+    model = BookReview
+    extra = 0
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['name', 'author', 'featured']
     list_filter = ['featured']
     readonly_fields = ['created']
+    inlines = [BookReviewInline]
 
 
 @admin.register(BookReview)
@@ -14,3 +21,5 @@ class BookReviewAdmin(admin.ModelAdmin):
     list_display = ['book', 'owner']
     list_filter = ['owner', 'book']
     readonly_fields = ['created']
+
+
