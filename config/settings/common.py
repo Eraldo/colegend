d04408e45ca113d23911008619b657196bd8118a
Eraldo Energy <eraldo@eraldo.org>
2018-02-22 22:55:16 +0100

    Updated server automation for changed server.

--- config/settings/common.py
+++ config/settings/common.py
@@ -275,7 +275,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_ROOT = env('STATIC_ROOT', default=str(ROOT_DIR('staticfiles')))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
--- fabfile.py
+++ fabfile.py
@@ -1,77 +1,171 @@
-from fabric.api import env, local, require
+from datetime import datetime
 
-
-def deploy():
-    """fab [environment] deploy"""
-    require('environment')
-    maintenance_on()
-    push()
-    migrate()
-    maintenance_off()
-    ps()
-
-
-def maintenance_on():
-    """fab [environment] maintenance_on"""
-    require('environment')
-    local('heroku maintenance:on --app %s' % env.app)
-
-
-def maintenance_off():
-    """fab [environment] maintenance_off"""
-    require('environment')
-    local('heroku maintenance:off --app %s' % env.app)
-
-
-def push():
-    """fab [environment] push"""
-    require('environment')
-    local('git push origin {branch}'.format(branch=env.branch))
-
-
-def migrate(app=None):
-    """fab [environment] migrate"""
-    require('environment')
-    if env.environment == "development":
-        if app is not None:
-            local('python manage.py migrate %s' % app)
-        else:
-            local('python manage.py migrate')
-    else:
-        if app is not None:
-            local('heroku run python manage.py migrate %s --app %s' % (app, env.app))
-        else:
-            local('heroku run python manage.py migrate --app %s' % env.app)
-
-
-def ps():
-    """fab [environment] ps"""
-    require('environment')
-    local('heroku ps --app %s' % env.app)
-
-
-def open():
-    """fab [environment] open"""
-    require('environment')
-    local('heroku open --app %s' % env.app)
+from fabric.api import cd, env, require, local, run
+from fabric.colors import blue
+from fabvenv import virtualenv
 
 
 def development():
     """fab development [command]"""
-    env.environment = 'development'
-    env.app = 'local'
+    env.name = 'development'
+    env.environment = env.name
+    # env.hosts = ['localhost']
+    env.path = './'
+    # env.virtualenv_path = 'workon colegend'
+    env.backup_path = './backups/development'
     env.branch = 'development'
+    env.push_branch = env.branch
+    env.push_remote = 'origin'
+    # env.reload_cmd = ''
+    env.db_name = 'colegend'
+    env.db_username = ''
+    env.after_deploy_url = 'http://127.0.0.1:8004/'
+    env.settings = '--settings=config.settings.local'
+    env.requirements = 'requirements/local.txt'
 
 
 def staging():
     """fab staging [command]"""
-    env.environment = 'staging'
-    env.app = 'colegend-staging'
+    env.name = 'staging'
+    env.environment = env.name
+    env.hosts = ['eraldo@eraldo.org']
+    env.path = 'staging.colegend.org/project'
+    env.virtualenv_path = 'staging.colegend.org/env'
+    env.backup_path = './backups/staging'
     env.branch = 'staging'
+    env.push_branch = env.branch
+    env.push_remote = 'origin'
+    env.start_cmd = 'svc -u colegendstaging'
+    env.stop_cmd = 'svc -d colegendstaging'
+    env.restart_cmd = 'svc -du colegendstaging'
+    env.db_name = 'colegendstaging'
+    env.db_username = 'colegend'
+    env.after_deploy_url = 'https://staging.colegend.org'
+    env.settings = '--settings=config.settings.production'
+    env.requirements = 'requirements/production.txt'
 
 
 def production():
     """fab production [command]"""
-    env.environment = 'production'
-    env.app = 'colegend'
+    env.name = 'production'
+    env.environment = env.name
+    env.hosts = ['eraldo@eraldo.org']
+    env.path = '~/colegend/colegend'
+    env.virtualenv_path = '~/colegend/env'
+    env.backup_path = '~/colegend/backups'
     env.branch = 'master'
+    env.push_branch = env.branch
+    env.push_remote = 'origin'
+    env.start_cmd = 'svc -u ~/service/gunicorn'
+    env.stop_cmd = 'svc -d ~/service/gunicorn'
+    env.restart_cmd = 'svc -du ~/service/gunicorn'
+    env.db_name = 'colegend'
+    env.db_username = 'colegend'
+    env.after_deploy_url = 'https://www.colegend.org'
+    env.settings = '--settings=config.settings.production'
+    env.requirements = 'requirements/production.txt'
+
+
+def backup():
+    """fab [environment] backup"""
+    if env.name == 'development':
+        local('pg_dump -Fc {db_name} '
+              '> {backup_path}/{db_name}-{environment}_`date +%Y-%m-%d_%H%M%S`.dump'.format(**env))
+    elif env.name in ['staging', 'production']:
+        with cd(env.backup_path):
+            env.timestamp = datetime.now().strftime('%Y-%m-%d_%H%M%S')
+            env.backup_file = '{backup_path}/{db_name}-{environment}_{timestamp}.dump'.format(**env)
+
+            print(blue('=> dumping database'))
+            run("pg_dump -Fc -U {db_username} {db_name} > {backup_file}".format(**env))
+
+        print(blue('=> downloading database'))
+        local('scp {hosts[0]}:{backup_file} backups/{environment}/'.format(**env))
+
+
+def migrate():
+    with virtualenv(env.virtualenv_path):
+        run("{path}/manage.py migrate {settings}".format(**env))
+
+
+def enable_debug():
+    with virtualenv(env.virtualenv_path):
+        run("sed -i -e 's/DJANGO_DEBUG=False/DJANGO_DEBUG=True/' {path}/.env".format(**env))
+    restart()
+
+
+def disable_debug():
+    with virtualenv(env.virtualenv_path):
+        run("sed -i -e 's/DJANGO_DEBUG=True/DJANGO_DEBUG=False/' {path}/.env".format(**env))
+    restart()
+
+
+def test():
+    """fab [environment] test"""
+    with virtualenv('colegend/env'):
+        print(blue('test'))
+        run('python')
+
+
+def logs():
+    """fab [environment] push"""
+    local('git push origin {push_branch}'.format(**env))
+
+
+def deploy():
+    with cd(env.path):
+        run('git pull {push_remote} {push_branch}'.format(**env))
+        with virtualenv(env.virtualenv_path):
+            run('pip install -Ur {requirements}'.format(**env))
+            run('./manage.py collectstatic --noinput {settings}'.format(**env))
+            run('cp staticfiles '.format(**env))
+            # run('./manage.py compilemessages %(settings)s'.format(**env))
+    migrate()
+    restart()
+    ping()
+
+
+def ping():
+    run('echo {after_deploy_url} returned:  '
+        '\>\>\>  $(curl --write-out %{{http_code}} --silent --output /dev/null {after_deploy_url})'.format(**env))
+
+
+def stop():
+    """fab [environment] stop"""
+    run(env.stop_cmd)
+
+
+def start():
+    """fab [environment] start"""
+    run(env.start_cmd)
+
+
+def restart():
+    """fab [environment] restart"""
+    run(env.restart_cmd)
+
+
+def push():
+    """fab [environment] push"""
+    local('git push origin {push_branch}'.format(**env))
+
+
+def ps():
+    """fab [environment] ps"""
+    run('htop')
+
+
+def logs():
+    """fab [environment] ps"""
+    run('zcat -f ~/service/gunicorn/log/main/* | tai64nlocal | less | tail')
+
+
+def open():
+    """fab [environment] open"""
+    local('open {after_deploy_url}'.format(**env))
+
+
+def create_superuser():
+    """fab [environment] create_superuser"""
+    with virtualenv(env.virtualenv_path):
+        run('{path}/manage.py createsuperuser {settings}'.format(**env))
