d6d6ce92c398f22745084835a5b2e725efd8d828
Eraldo Energy <eraldo@eraldo.org>
2018-03-22 15:56:28 +0100

    Rejecting inactive users.

--- colegend/api/middelware.py
+++ colegend/api/middelware.py
@@ -1,13 +1,16 @@
 from rest_framework.authtoken.models import Token
 
 
-class AuthorizationMiddleware(object):
-
+class AuthorizationMiddleware:
+    """
+    Add the authenticated user to the context.
+    (Unless the user is not active.)
+    """
     def resolve(self, next, root, info, **args):
         if not info.context.user.is_authenticated:
             token = info.context.META.get('HTTP_AUTHORIZATION')
             if token:
                 user = Token.objects.select_related('user').get(key=token).user
-                if user:
+                if user and user.is_active:
                     info.context.user = user
         return next(root, info, **args)
