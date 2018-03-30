76d4b95aab809e57d72aa0567d28c3297f8146cf
Eraldo Energy <eraldo@eraldo.org>
2018-03-30 14:38:30 +0200

    Added filter dropdowns to django admin.

--- colegend/donations/admin.py
+++ colegend/donations/admin.py
@@ -5,4 +5,4 @@ from .models import Donation
 @admin.register(Donation)
 class DonationAdmin(admin.ModelAdmin):
     list_display = ['date', 'amount', 'owner']
-    list_filter = ['owner']
+    list_filter = ['date', 'owner']
