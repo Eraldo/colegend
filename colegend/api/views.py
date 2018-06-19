bdb12eb9d0184100bb8a3d7becdb02b76b268e29
Eraldo Energy <eraldo@eraldo.org>
2018-06-19 21:32:29 +0200

    [wip] file uploads.

--- colegend/api/views.py
+++ colegend/api/views.py
@@ -2,6 +2,8 @@
 #
 # import six
 # from django.http import HttpResponseBadRequest
+import json
+
 from graphene_django.views import GraphQLView as GrapheneGraphQLView
 from rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
@@ -20,6 +22,18 @@ class GraphQLView(GrapheneGraphQLView):
     """
     Custom version of GraphQLView to handle file uploads.
     """
+    @staticmethod
+    def get_graphql_params(request, data):
+        """
+        Workaround for createUploadLink
+        :param request:
+        :param data:
+        :return:
+        """
+        if 'operations' in request.POST:
+            data = json.loads(request.POST.get('operations'))
+        return GrapheneGraphQLView.get_graphql_params(request, data)
+
     # @staticmethod
     # def get_graphql_params(request, data):
     #     print(request.GET, request.POST, data)
--- colegend/lab/schema.py
+++ colegend/lab/schema.py
@@ -6,24 +6,24 @@ class Upload(graphene.Scalar):
         pass
 
 
-# class UploadFiles(graphene.relay.ClientIDMutation):
-#     success = graphene.Boolean()
-#
-#     class Input:
-#         files = Upload()
-#
-#     @staticmethod
-#     def mutate_and_get_payload(cls, info, files=None):
-#         # # TODO: Remove breakpoint
-#         # import ipdb; ipdb.set_trace()
-#         print(cls, files)
-#         files = info.context.FILES
-#         print(files)
-#         # files = context.files
-#         # client_signature = files['variables.signature']
-#         return UploadFiles(success=True)
+class UploadFiles(graphene.relay.ClientIDMutation):
+    success = graphene.Boolean()
+
+    class Input:
+        files = Upload()
+
+    @staticmethod
+    def mutate_and_get_payload(cls, info, files=None):
+        # # TODO: Remove breakpoint
+        import ipdb; ipdb.set_trace()
+        print(cls, files)
+        files = info.context.FILES
+        print(files)
+        # files = context.files
+        # client_signature = files['variables.signature']
+        return UploadFiles(success=True)
 
 
 class LabMutations(graphene.ObjectType):
-    pass
-    # upload = UploadFiles.Field()
+    # pass
+    upload = UploadFiles.Field()
