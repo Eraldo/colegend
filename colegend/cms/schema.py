e83a1e0db2439fc5eb279180ecc53be5ddd8aab9
Eraldo Energy <eraldo@eraldo.org>
2018-07-22 17:22:50 +0200

    Added page queries by name.

--- colegend/cms/schema.py
+++ colegend/cms/schema.py
@@ -3,6 +3,7 @@ from graphene import Scalar
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.converter import convert_django_field
+from graphql_relay import from_global_id
 from wagtail.core.fields import StreamField
 
 from colegend.api.models import CountableConnectionBase
@@ -35,8 +36,21 @@ class ContentPageNode(DjangoObjectType):
 
 
 class PageQuery(graphene.ObjectType):
-    content_page = graphene.Node.Field(ContentPageNode)
+    content_page = graphene.Field(ContentPageNode, id=graphene.String(), slug=graphene.String())
     content_pages = DjangoFilterConnectionField(ContentPageNode)
+
+    def resolve_content_page(self, info, **kwargs):
+        id = kwargs.get('id')
+        slug = kwargs.get('slug')
+
+        if id is not None:
+            _type, id = from_global_id(id)
+            return ContentPage.objects.get(id=id)
+
+        if slug is not None:
+            return ContentPage.objects.get(slug=slug)
+
+        return ContentPage.objects.none()
 
 
 class PageMutation(graphene.ObjectType):
